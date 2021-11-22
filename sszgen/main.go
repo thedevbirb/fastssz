4892782e33de65c1b8c9dbc4b20b2545732ade9d
rkapka <rkapka@wp.pl>
2021-11-22 15:59:23 +0100

    refactor

--- sszgen/main.go
+++ sszgen/main.go
@@ -436,23 +436,18 @@ func (e *env) print(first bool, order []string, experimental bool) (string, bool
 		if experimental {
 			getTree = e.getTree(name, obj)
 		}
-		if len(obj.opts) == 1 && obj.opts[0] == "no-htr" {
-			objs = append(objs, &Obj{
-				HashTreeRoot: "",
-				GetTree:      getTree,
-				Marshal:      e.marshal(name, obj),
-				Unmarshal:    e.unmarshal(name, obj),
-				Size:         e.size(name, obj),
-			})
-		} else {
-			objs = append(objs, &Obj{
-				HashTreeRoot: e.hashTreeRoot(name, obj),
-				GetTree:      getTree,
-				Marshal:      e.marshal(name, obj),
-				Unmarshal:    e.unmarshal(name, obj),
-				Size:         e.size(name, obj),
-			})
+		o := &Obj{
+			GetTree:      getTree,
+			Marshal:      e.marshal(name, obj),
+			Unmarshal:    e.unmarshal(name, obj),
+			Size:         e.size(name, obj),
 		}
+		if len(obj.opts) == 1 && obj.opts[0] == "no-htr" {
+			o.HashTreeRoot = ""
+		} else {
+			o.HashTreeRoot= e.hashTreeRoot(name, obj)
+		}
+		objs = append(objs, o)
 	}
 	if len(objs) == 0 {
 		// No valid objects found for this file
