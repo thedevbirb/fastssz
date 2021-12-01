ddabdd7a9ce432243feca5b76246e618dfaf3676
rkapka <rkapka@wp.pl>
2021-12-01 17:57:04 +0100

    t1

--- sszgen/main.go
+++ sszgen/main.go
@@ -436,18 +436,13 @@ func (e *env) print(first bool, order []string, experimental bool) (string, bool
 		if experimental {
 			getTree = e.getTree(name, obj)
 		}
-		o := &Obj{
+		objs = append(objs, &Obj{
+			HashTreeRoot: e.hashTreeRoot(name, obj),
 			GetTree:      getTree,
 			Marshal:      e.marshal(name, obj),
 			Unmarshal:    e.unmarshal(name, obj),
 			Size:         e.size(name, obj),
-		}
-		if len(obj.opts) == 1 && obj.opts[0] == "no-htr" {
-			o.HashTreeRoot = ""
-		} else {
-			o.HashTreeRoot= e.hashTreeRoot(name, obj)
-		}
-		objs = append(objs, o)
+		})
 	}
 	if len(objs) == 0 {
 		// No valid objects found for this file
